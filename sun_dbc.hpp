c4f7d2580f435cfa083466bff018e5b79cf74258
sunzcosmo <suncosmo@163.com>
2019-05-07 16:29:57 +0800

    Add database connector hpp file;

---
+++ sun_dbc.hpp
@@ -0,0 +1,20 @@
+#ifndef _SUN_DBC_HPP_
+#define _SUN_DBC_HPP_
+
+namespace sun {
+
+namespace dbc {
+
+class DatabaseConnector {
+
+}; // class DatabaseConnector
+
+class MysqlDatabaseConnector : public DatabaseConnector {
+
+}; // class MysqlDatabaseConnector
+
+} // namespace dbc
+
+} // namespace sun
+
+#endif //_SUN_DBC_HPP_
